3363feae289aac1829f237dd817f09dfcdaa80a4
xuyixin <xuyixin0723@gmail.com>
2017-11-14 20:58:50 +0800

    2nd commit, realize zoom/move/pan functions of CompsView

--- src/pcb_view/views/compsView.js
+++ src/pcb_view/views/compsView.js
@@ -1,45 +1,60 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {ReactSVGPanZoom} from 'react-svg-pan-zoom';
 
-import Comp from './comp'
-// import {FilterTypes} from '../../constants.js';
+import Comp from './comp';
 
-const CompsView = ({comps}) => {
-  return (
-    <svg width={1500} height={1000}>
-      {comps.map((comp) => (
-        <Comp
-          key={comp.id}
-          id={comp.id}
-          name={comp.name}
-          x={comp.x}
-          y={comp.y}
-          width={comp.width}
-          height={comp.height}
-          result={comp.result}
-        />
-        ))
-      }
-    </svg>
-  );
-};
 
-CompsView.propTypes = {
-  comps: PropTypes.array.isRequired
-};
+class CompsView extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.Viewer = null;
+  }
+  componentDidMount() {
+    this.Viewer.fitToViewer();
+  }
+  render() {
+    const {comps} = this.props; //解构赋值
+    return (
+      <div>
+        <button onClick={event => this.Viewer.zoomOnViewerCenter(1.1)}>Zoom in</button>
+        <button onClick={event => this.Viewer.fitSelection(40, 40, 200, 200)}>Zoom area</button>
+        <button onClick={event => this.Viewer.fitToViewer()}>Fit</button>
 
-// const selectVisibleTodos = (todos, filter) => {   switch (filter) {     case
-// FilterTypes.ALL:       return todos;     case FilterTypes.COMPLETED:
-// return todos.filter(item => item.completed);     case
-// FilterTypes.UNCOMPLETED:       return todos.filter(item => !item.completed);
-//    default:       throw new Error('unsupported filter');   } }
+        <hr/>
 
-const mapStateToProps = (state) => {
-  return {comps: state.comps}
+        <ReactSVGPanZoom
+          style={{border: "1px solid black"}}
+          width={500} height={500} ref={Viewer => this.Viewer = Viewer}
+          onClick={event => console.log('click', event.x, event.y, event.originalEvent)}
+          onMouseUp={event => console.log('up', event.x, event.y)}
+          onMouseMove={event => console.log('move', event.x, event.y)}
+          onMouseDown={event => console.log('down', event.x, event.y)}>
+
+          <svg width={900} height={800}>
+            {comps.map((comp) => (
+                <Comp
+                    key={comp.id}
+                    id={comp.id}
+                    name={comp.name}
+                    x={comp.x}
+                    y={comp.y}
+                    width={comp.width}
+                    height={comp.height}
+                    result={comp.result}
+                />
+                ))
+            }
+          </svg>
+        </ReactSVGPanZoom>
+      </div>
+    );
+  }
 }
 
-// const mapStateToProps = (state) => {   return {
-//     todos: selectVisibleTodos(state.todos, state.filter)   }; }
-
+const mapStateToProps = (state) => {
+    return {comps: state.comps}
+}
+  
 export default connect(mapStateToProps)(CompsView);
